cf75f3579093974d4bce9cb68768b5bd8904b52c
Petar Baykov <petbaik26@gmail.com>
2020-05-07 13:09:18 +0300

    Add address attribute

--- widget.js
+++ widget.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   for (var i = 0; i < buttons.length; i++) {
     var size = buttons[i].getAttribute("data-size") ? buttons[i].getAttribute("data-size") : "icon";
     var href = buttons[i].getAttribute("data-href") ? buttons[i].getAttribute("data-href") : window.location.href;
-    
+    var address = buttons[i].getAttribute("data-address");
+
     var iframe = document.createElement('iframe');
     iframe.src = buttonUrl + size + "?url=" + href;
     iframe.setAttribute('style','max-width:210px;max-height:120px;overflow:hidden;');
     iframe.setAttribute("scrolling", "no");
     iframe.setAttribute("frameborder", "0");
+    if (address) iframe.setAttribute("data-address", address);
     iframes[i] = iframe;
   }
 
