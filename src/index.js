e6fb271169f0865293e3b08740fdcd8f12a20916
Milen Radkov <milenradkov@me.com>
2020-10-21 17:08:10 +0300

    fix typo: payed -> paid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './global.scss';
 export { default as createButton } from './button';
-export { default as ensurePayed } from './paywall';
+export { default as ensurePaid } from './paywall';
+
