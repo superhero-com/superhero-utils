f6b0b5eb5b39ccf8bebafc03ff3bf295b6116e61
Yury Shapkarin <yury@shapkarin.me>
2020-12-01 21:14:44 +0300

    add all button sizes for the vue example

--- examples/vue-webpack/src/App.vue
+++ examples/vue-webpack/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
+  <div class="Example-List">
     <h3>superhero-utils example using vue, webpack</h3>
+    medium:
     <SuperheroButton size="medium" account="ak_... or .chain name" />
+    <template v-for="size in ['large', 'small', 'icon']">
+      {{size}}:
+      <SuperheroButton :size="size" account="ak_... or .chain name" :key="size" />
+    </template>
   </div>
 </template>
 
@@ -15,3 +20,30 @@ export default {
   },
 }
 </script>
+
+<style>
+.Example-List {
+  width: 235px;
+  outline: 1px #1162fe solid;
+  padding: 20px;
+  height: 400px;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-flex-wrap: nowrap;
+  -ms-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  -webkit-justify-content: space-between;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  -webkit-align-content: stretch;
+  -ms-flex-line-pack: stretch;
+  align-content: stretch;
+  -webkit-align-items: flex-start;
+  -ms-flex-align: start;
+  align-items: flex-start;
+}
+</style>
