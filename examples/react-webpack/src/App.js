c87d8bc3fcac8d333c349373d6bfd8b49f8074e9
Yury Shapkarin <yury@shapkarin.me>
2020-12-01 20:51:31 +0300

    add all button sizes for the react example

--- examples/react-webpack/src/App.js
+++ examples/react-webpack/src/App.js
@@ -1,11 +1,21 @@
 import './superhero-utils/index.css';
 import { Button as SuperheroButton } from './superhero-utils/react-without-styles.js';
 
+import './style.css';
+
 function App() {
   return (
-    <div>
+    <div className="Example-List">
       <h3>superhero-utils example using react, webpack</h3>
+      medium:
       <SuperheroButton size="medium" account="ak_... or .chain name" />
+
+      {['large', 'small', 'icon'].map(size =>
+        <>
+          {size}:
+          <SuperheroButton size={size} account="ak_... or .chain name" />
+        </>
+      )}
     </div>
   );
 }
