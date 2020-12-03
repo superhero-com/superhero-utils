ba06fbad5f6f894ed6936992d0c44ea56dfe9b69
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-03 03:52:09 +0000

    Deploy aeternity/superhero-utils to github.com/aeternity/superhero-utils.git:gh-pages

--- examples/vue-webpack/vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: '/vue-webpack/dist/',
-};
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './global.scss';
-export { default as createButton, createButtonByDiv } from './button';
-export { default as ensurePaid } from './paywall';
